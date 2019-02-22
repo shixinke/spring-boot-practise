b2b578b4416e9bb624eacd2c604671e0683988fa
shixinke <ishixinke@qq.com>
2019-02-22 13:46:02 +0800

    practise init

---
+++ apollo/src/main/java/com/github/shixinke/practise/apollo/ApolloPractiseApplication.java
@@ -0,0 +1,13 @@
+package com.github.shixinke.practise.apollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApolloPractiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApolloPractiseApplication.class, args);
+    }
+
+}
---
+++ sentinel/src/main/java/com/github/shixinke/practise/sentinel/SentinelClientApplication.java
@@ -0,0 +1,13 @@
+package com.github.shixinke.practise.sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelClientApplication.class, args);
+    }
+
+}
